024a3b3618fdd35bd3821f060b243d2244dd07b1
IlyaShpilko <61794412+IlyaShpilko@users.noreply.github.com>
2021-06-13 15:33:37 +0400

    docs: first task completed

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,38 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var arr = prices
+        var index = 0
+        var result = 0
+        
+        if arr.isEmpty || arr.count == 1 || (arr.count == 2 && arr[0] == arr.max()) {
+            return 0
+        }
+        
+        if arr.count > 2 && arr[0] == arr.max() {
+            arr.remove(at: 0)
+        
+        }
+        
+        if arr[arr.count-2] == arr.max() {
+            arr.removeLast()
+            arr.removeLast()
+        }
+        
+        for i in arr {
+            
+            guard let max = arr.max() else { return 0 }
+            
+            if arr[arr.count - 1] == max {
+                result += max - i
+            } else {
+                result += max - i
+                arr.remove(at: index)
+                index -= 1
+            }
+            index += 1
+        }
+        
+        return result
     }
 }
