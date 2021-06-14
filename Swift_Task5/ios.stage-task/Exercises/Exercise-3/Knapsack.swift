f5fea19e1e9b670bcf0222436c5f73c1609a9a2a
IlyaShpilko <61794412+IlyaShpilko@users.noreply.github.com>
2021-06-14 14:55:05 +0400

    docs: third task completed

--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -16,7 +16,48 @@ public final class Knapsack {
         self.foods = foods
     }
     
+    func countingArrays(_ array: [Supply]) -> [[Int]] {
+        
+        var helpVar = Array(repeating: Array(repeating: 0, count: maxWeight+1), count: array.count+1)
+        
+        for i in 0...array.count {
+            
+            for j in 0...maxWeight {
+                
+                if i != 0 && j != 0 {
+                    
+                    if array[i-1].weight > j {
+                        helpVar[i][j] = helpVar[i - 1][j]
+                        
+                    } else {
+                        
+                        let oneVariable = helpVar[i-1][j]
+                        let twoVariable = array[i-1].value + helpVar[i-1][j - array[i - 1].weight]
+                        
+                        helpVar[i][j] = max(oneVariable, twoVariable)
+                    }
+                }
+            }
+        }
+        return helpVar
+    }
+    
     func findMaxKilometres() -> Int {
-        return -1
+        guard maxWeight <= 2500 && maxWeight > 0 else { return 0 }
+        
+        let foodArray = countingArrays(foods)
+        let drinkArray = countingArrays(drinks)
+        var distatance = 0
+
+        for i in 0...maxWeight {
+            
+            let minFood = foodArray[foods.count][i]
+            let minDrink = drinkArray[drinks.count][maxWeight-i]
+            let minVar = min(minFood, minDrink)
+            
+            distatance = max(distatance, minVar)
+        }
+        
+        return distatance
     }
 }
