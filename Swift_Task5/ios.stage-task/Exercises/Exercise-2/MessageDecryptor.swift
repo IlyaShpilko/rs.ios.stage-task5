6314ff72c9ec591e20eb6fdcf6e46ed9ac5dece7
IlyaShpilko <61794412+IlyaShpilko@users.noreply.github.com>
2021-06-14 13:49:19 +0400

    docs: second task completed

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,54 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        
+        var numberArray = [String]()
+        var stringArray = [String]()
+        var result = ""
+        var string = ""
+        
+        for char in message {
+            
+            if char.isNumber {
+                
+                string = string + String(char)
+                
+            } else if char == "[" {
+                
+                if string.isEmpty {
+                    
+                    numberArray.append("1")
+                    
+                } else {
+                    
+                    numberArray.append(string)
+                    
+                }
+                
+                string = ""
+                stringArray.append(result)
+                result = ""
+                
+            } else if char == "]" {
+                var helpString = ""
+                
+                if let number = Int(numberArray.popLast() ?? "1") {
+                    
+                    for _ in 0..<number {
+                        helpString += result
+                    }
+                }
+                
+                guard let newStringArray = stringArray.popLast() else { return "" }
+                
+                result = newStringArray + helpString
+                
+            } else if char.isLetter {
+                result.append(char)
+            }
+        }
+        
+        return result
     }
+
 }
